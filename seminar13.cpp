55c65f3a2fe318c281993027bb09b222d9a84fda
andreeas4 <164545355+andreeas4@users.noreply.github.com>
2025-01-20 11:03:19 +0200

    Update seminar13.cpp

--- seminar13.cpp
+++ seminar13.cpp
@@ -1,43 +1,57 @@
-#include<iostream>
-#include<map>
+//definim o clasa persoana
+// shiftare pt afisare
+// container asociativ intre pers si marca
+
+#include <iostream>
+#include <map>
+
+
 using namespace std;
 
 class persoana
 {
 	int marca;
-	int varsta;
 	string nume;
+	int varsta;
 public:
-	persoana(int fmarca, int fvarsta, string fnume) :marca(fmarca), varsta(fvarsta), nume(fnume) {}
+	persoana(int fm = 0, string fnume = "N/A", int fv = 0) :marca(fm), nume(fnume), varsta(fv) {}
+	string get_nume() { return nume; }
 	int get_marca() { return marca; }
 	int get_varsta() { return varsta; }
-	string get_nume() { return nume; }
-
-	friend ostream& operator<<(ostream& os, persoana& p)
+	friend ostream& operator << (ostream& os, persoana& pers)
 	{
-		os << endl;
-		os << "Marca: " << p.marca << endl;
-		os << "Varsta:" << p.varsta << endl;
-		os << "Numele:" << p.nume << endl;
+		os << "( " << pers.get_marca() << " " << pers.get_nume() << " " << pers.get_varsta() << " )";
 		return os;
 	}
 };
 
 void main()
 {
-	persoana vp[] = { persoana(100,32,"Ana"),persoana(500,23,"Maria"),persoana(250,37,"Mihai") };
-	map<int, persoana,greater<int>> mp;
+
+	int m;
+	persoana p1(11, "andrei", 24), p2, p3(1, "afro", 14);
+	persoana vp[] = { persoana(11,"andrei",24) ,persoana(),persoana(1,"afro",14) };
+	//for (int i = 0; i < 3; i++) cout << vp[i];
+	map <int, persoana> mp;
 	for (int i = 0; i < sizeof(vp) / sizeof(persoana); i++)
 	{
-		pair<int, persoana> el(vp[i].get_marca(), vp[i]);
-		mp.insert(el);
+		//pair <int, persoana> el(vp[i].get_marca(), vp[i]);
+		mp.insert(pair <int, persoana>(vp[i].get_marca(), vp[i]));
 	}
-	map<int, persoana> ::iterator mit;
+	map <int, persoana>::iterator mit;
 	for (mit = mp.begin(); mit != mp.end(); mit++)
-		cout << mit->first <<" " << mit->second<<endl;
+	{
+		cout << "[ " << mit->first << " " << mit->second << " ]" << endl;
+	}
+	cout << "Marca pers cautata: "; cin >> m;
+	mit=mp.find(m);
+	if (mit == mp.end())//nu a fost gasit
+		cout << "Pers cu marca " << m << " nu a fost gasita ";
+	else cout << mit->second << endl;
 
-	mit = mp.find(250);
-	
-
-
+	//mp.insert(el1); mp.insert(el2); mp.insert(el3);
+	//map <int, string>::iterator mit;
+	//for (mit)
 }
+
+
