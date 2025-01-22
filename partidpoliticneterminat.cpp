1f0d484a167d31f12fd613150aba2aa3266c82be
andreeas4 <164545355+andreeas4@users.noreply.github.com>
2025-01-22 13:38:25 +0200

    Update partidpoliticneterminat.cpp

--- partidpoliticneterminat.cpp
+++ partidpoliticneterminat.cpp
@@ -1,61 +1,92 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cstring> // Pentru strncpy
 
 using namespace std;
 
+// Clasa de bază Persoana
 class Persoana {
 private:
-    char cnp[11];
-    string nume;
+    char cnp[14]; // CNP-ul persoanei, lungime fixa
+    string nume;  // Numele persoanei
 
 public:
-    // Constructor
-    Persoana(const char fcnp[10], string fnume = "Anonim") : nume(fnume) {
-        if (strlen(fcnp) == 10)
-        {
-            strncpy(cnp, fcnp, 10);
-            cnp[10] = '\0';
+    // Constructor cu parametri
+    Persoana(const char fcnp[14], string fnume = "Anonim") : nume(fnume) {
+        if (strlen(fcnp) <= 13) {
+            strncpy(cnp, fcnp, 14); // Copiem CNP-ul
+            cnp[14] = '\0';        // Asiguram terminarea sirului
         }
         else {
-            strcpy(cnp, "N/0");
-            cout << "Nu exista o persoana cu acest Cnp" << endl;
-        }
-    }
-    Persoana(Persoana &other):nume(other.nume){
-        if (strlen(other.cnp) == 10)
-        {
-            strncpy(cnp, other.cnp, 10);
-            cnp[10] = '\0';
-        }
-        else {
-            strcpy(cnp, "N/0");
-            cout << "Nu exista o persoana cu acest Cnp" << endl;
+            strcpy(cnp, "N/A"); // CNP invalid
+            cout << "CNP invalid!" << endl;
         }
     }
 
-    const char* getCnp(){   return cnp;  }
-    string getNume() {  return nume;    }
-    void setNume(string newName) {  nume = newName;}
-    virtual void afiseaza(ostream &os) const {   os << "CNP: " << cnp << ", Nume: " << nume << endl; }
-    friend ostream& operator <<(ostream& os, const Persoana& p) {
-        p.afiseaza(os);//POLIMORFISM DINAMIC
+    // Constructor de copiere
+    Persoana(const Persoana& other) : nume(other.nume) {
+        strncpy(cnp, other.cnp, 10);
+        cnp[10] = '\0';
+    }
+
+    // Accesori
+    const char* getCnp() const { return cnp; }
+    string getNume() const { return nume; }
+
+    void setNume(string newName) { nume = newName; }
+
+    // Metoda virtuala pentru afisare - baza polimorfismului
+    virtual void afiseaza(ostream& os) const {
+        os << "CNP: " << cnp << ", Nume: " << nume;
+    }
+
+    // Supraincarcare operator << (polimorfism dinamic prin metoda virtuala)
+    friend ostream& operator<<(ostream& os, const Persoana& p) {
+        p.afiseaza(os); // Apel polimorf la metoda afiseaza
         return os;
     }
 };
 
-class PartidPolitic :public Persoana {
+// Clasa derivata PartidPolitic
+class PartidPolitic : public Persoana {
 private:
-    float cotizatie;
+    float cotizatie; // Cotizatia platita de membrii partidului
+
 public:
-    PartidPolitic(char)
+    // Constructor care apeleaza constructorul clasei de baza
+    PartidPolitic(const char fcnp[14], string fnume, float fcotizatie)
+        : Persoana(fcnp, fnume), cotizatie(fcotizatie) {}
+
+    // Suprascrierea metodei virtuale pentru afisare
+    void afiseaza(ostream& os) const override {
+        // Apelam metoda afiseaza din clasa de baza
+        Persoana::afiseaza(os);
+        os << ", Cotizatie: " << cotizatie;
+    }
 };
 
+int main() {
+    // Obiect valid
+    Persoana p3("1234567890123", "Ion Popescu");
+    cout << p3 << endl;
 
-void main()
-{
-    Persoana p1("4034562089", "Mariana Popescu");
+    // Obiect cu CNP invalid
+    Persoana p2("12345", "Invalid CNP");
+    cout << p2 << endl;
 
-    cout << p1;
+    // Obiect de tip Persoana
+    Persoana p1("123456789000", "Ion Popescu");
 
+    // Obiect de tip PartidPolitic
+    PartidPolitic pp("09876543210001", "Maria Ionescu", 250.5);
 
+    // Demonstratie de polimorfism:
+    cout << "Afisare obiect de tip Persoana:" << endl;
+    cout << p1 << endl; // Apeleaza afiseaza() din Persoana
+
+    cout << "Afisare obiect de tip PartidPolitic:" << endl;
+    cout << pp << endl; // Apeleaza afiseaza() din PartidPolitic (polimorfism dinamic)
+
+    return 0;
 }
 
